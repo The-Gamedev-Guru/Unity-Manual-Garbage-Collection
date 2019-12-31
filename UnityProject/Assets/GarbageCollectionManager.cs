6c83c0fd154cc75fb445f11032145f5156cba3f6
rubentorresbonet <cofruben@gmail.com>
2019-12-31 18:45:49 +0100

    The good stuff

---
+++ UnityProject/Assets/GarbageCollectionManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace TheGamedevGuru
+{
+public class GarbageCollectionManager : MonoBehaviour
+{
+	[SerializeField] private float maxTimeBetweenGarbageCollections = 60f;
+	private float _timeSinceLastGarbageCollection;
+
+	private void Start()
+	{
+#if !UNITY_EDITOR
+      GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
+#endif
+		// You might want to run this during loading times, screen fades and such.
+		// Events.OnScreenFade += CollectGarbage;
+	}
+
+	private void Update()
+	{
+		_timeSinceLastGarbageCollection += Time.unscaledDeltaTime;
+		if (_timeSinceLastGarbageCollection > maxTimeBetweenGarbageCollections)
+		{
+			CollectGarbage();
+		}
+	}
+
+	private void CollectGarbage()
+	{
+		_timeSinceLastGarbageCollection = 0f;
+		Debug.Log("Collecting garbage"); // talking about garbage... 
+#if !UNITY_EDITOR
+      // Not supported on the editor
+      GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
+      GC.Collect();
+      GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
+#endif
+	}
+}
+}
---
+++ UnityProject/Assets/GarbageCreator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TheGamedevGuru
+{
+public class GarbageCreator : MonoBehaviour
+{
+	[SerializeField] private int garbageCreationRate = 1024;
+	private static int[] _garbage;
+	void Update()
+	{
+		_garbage = new int[garbageCreationRate];
+	}
+}
+}
